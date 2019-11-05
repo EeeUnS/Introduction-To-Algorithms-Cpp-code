5d71800236bbd2ab66b3e55de6d1d46f2ab9ec3b
EeeUnS <42970615+EeeUnS@users.noreply.github.com>
2019-11-05 16:22:54 +0900

    Update 15_4.cpp

--- 15DP/15_4.cpp
+++ 15DP/15_4.cpp
@@ -5,6 +5,7 @@
 #include<vector>
 #include<windows.h>
 
+#include<string.h>
 enum class arrow
 {
     left_up ,
@@ -12,12 +13,14 @@ enum class arrow
     left
 };
 
-void LCS_LENGTH(const std::vector<int> &X,const std::vector<int> &Y )
+typedef std::pair<std::vector<std::vector<int>>,std::vector<std::vector<arrow>>> Set;
+
+Set LCS_LENGTH(const char* XX,const char* YY , const int m, const int n)
 {
-    const int m = X.size();
-    const int n = Y.size();
-    std::vector<std::vector<int>> b(n+1,std::vector<int>(m+1,0)) ;
-    std::vector<std::vector<int>> c(n+1,std::vector<int>(m+1,0)) ;
+    const char * X = XX-1;
+    const char * Y = YY-1;
+    std::vector<std::vector<arrow>> b(m+1,std::vector<arrow>(n+1,arrow::left_up)) ;
+    std::vector<std::vector<int>> c(m+1,std::vector<int>(n+1,0)) ;
     for(int i = 1 ; i <= m ; i++)
     {
         c[i][0] = 0;
@@ -26,15 +29,16 @@ void LCS_LENGTH(const std::vector<int> &X,const std::vector<int> &Y )
     {
         c[0][j] = 0;
     }
+
     for(int i = 1 ; i <= m; i++)
     {
 
         for(int j = 1; j <= n;j++)
         {
-            if(x[i] == Y[j])
+            if(X[i] == Y[j])
             {
                 c[i][j] = c[i-1][j-1] +1;
-                b[i][j] =arrow::left_up;
+                b[i][j] = arrow::left_up;
             }else if(c[i-1][j] >= c[i][j-1])
             {
                 c[i][j] = c[i-1][j];
@@ -47,27 +51,50 @@ void LCS_LENGTH(const std::vector<int> &X,const std::vector<int> &Y )
             }
         }
     }
+    return std::make_pair(c,b);
 }
 
 
-void PRINT_LCS(const std::vector<int> & b , const std::vector<int> &X, int i , int j)
+void PRINT_LCS(const std::vector<std::vector<arrow>> b ,const char* XX, int i , int j)
 {
+    const char * X = XX-1;
     if( i == 0 || j == 0 )
     {
         return;
     }
     if( b[i][j] == arrow::left_up)
     {
-        PRINT_LCS(b, X, i-1 , j-1);
-        std::cout << x[i] <<' ';
+        PRINT_LCS(b, X+1, i-1 , j-1);
+        std::cout << X[i] <<' ';
     }
     else if(b[i][j] == arrow::up)
     {
-        PRINT_LCS(b,X,i-1, j);
+        PRINT_LCS(b,X+1,i-1, j);
     }
     else
-        PRINT_LCS(b,X,i, j-1);
+        PRINT_LCS(b,X+1,i, j-1);
 }
 
+const char *X = "ACCGGTCGAGTGCGCGGAAGCCGGCCGAA";
+const char *Y = "GTCGTTCGGAATGCCGTTGCTCTGTAAA";
 
+//const char *X = "ABCBDAB";
+//const char *Y = "BDCABA";
+char *Z = "GTCGTCGGAAGCCGGCCGAA";
 
+int main()
+{
+    const int m = strlen(X);
+    const int n = strlen(Y);
+    Set cb = LCS_LENGTH(X,Y,m,n);
+
+    for(int i = 0 ; i <= m ; i++)
+    {
+        for(int j = 0 ; j <= n ; j++)
+        {
+            std::cout <<cb.first[i][j] <<' ';
+        }
+        std::cout <<'\n';
+    }
+    PRINT_LCS(cb.second, X, m, n );
+}
